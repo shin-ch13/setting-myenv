63b8047d5f5cde388dbe20805ce66f609c070704
shin-ch13 <soccer.nobu@gmail.com>
2018-05-06 17:19:58 +0900

    edit provisionig

--- provisioning/config.sh
+++ provisioning/config.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 read -p "Please hostname: " hostname
-find ./hosts ./config.sh -type f -print0 | xargs -0 sed -i '' -e "s/172.21.1.7/$hostname/g"
+find ./hosts ./config.sh -type f -print0 | xargs -0 sed -i '' -e "s/XXX/$hostname/g"
 read -p "Please username: " username
-find . -type f -print0 | xargs -0 sed -i '' -e "s/nobu/$username/g"
+find . -type f -print0 | xargs -0 sed -i '' -e "s/vagrant/$username/g"
 while true; do
     read -p 'Do you want to run the ansible?[y/n]' yn
     case $yn in
