e9e723dab6ddce4a064fa0d7ac353fbb54d59570
Nobuhiro Kaneko <kanekonobuhiro@mbp.local>
2020-05-10 16:28:03 +0900

    fix playbook for ansible 2.9.7 and add docker playbook

---
+++ provisioning/ansible_execute.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+vi ./inventory/inventory.ini
+while true; do
+    read -p 'Do you want to run the ansible?[y/n]' yn
+    case $yn in
+        [Yy] ) break;;
+        [Nn] ) exit;;
+        * ) echo "[y/n]で入力してください";;
+    esac
+done
+ansible-playbook site.yml -i ./inventory/inventory.ini -u $username -K
