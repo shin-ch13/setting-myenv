4c4b2a7cdbd616d0e7a3e30e849d5a643dda8ace
shin-ch13 <kanekonobuhiro@mbp.local>
2020-09-25 22:04:06 +0900

    ansible_execute.sh edit zsh tasks by vi

--- provisioning/ansible_execute.sh
+++ provisioning/ansible_execute.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 vi ./inventory/inventory.ini
+vi ./roles/zsh_ubuntu/tasks/main.yml
+vi ./roles/zsh_centos/tasks/main.yml
+
 while true; do
     read -p 'Do you want to run the ansible?[y/n]' yn
     case $yn in
@@ -8,4 +11,4 @@ while true; do
         * ) echo "[y/n]で入力してください";;
     esac
 done
-ansible-playbook site.yml -i ./inventory/inventory.ini -u $username --ask-pass --ask-become-pass
+ansible-playbook site.yml -i ./inventory/inventory.ini -u nobu --ask-pass --ask-become-pass
